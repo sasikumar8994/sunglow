607065102a1a00d5f8ccd9389dd23d20d1845254
Anbazhagan <sasikumar8994@gmail.com>
2017-05-29 01:25:25 +0530

    mail production setting changes

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -14,13 +14,13 @@ if Rails.env.development?
   ActionMailer::Base.default_url_options[:host] = "http://localhost:3000"
 else
   ActionMailer::Base.smtp_settings = {
-    :address   => "smtp.mandrillapp.com",
+    :address   => "smtp.gmail.com",
     :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :domain => 'gmail.com', # your domain to identify your server when connecting
+    :authentication => 'plain', # Mandrill supports 'plain' or 'login'
     :enable_starttls_auto => true, # detects and uses STARTTLS
     :user_name => "dummytestttt@gmail.com",
     :password  => "unleashedblacklove", # SMTP password is any valid API key
-    :authentication => 'plain', # Mandrill supports 'plain' or 'login'
-    :domain => 'herokuapp.com', # your domain to identify your server when connecting
   }
   ActionMailer::Base.default_url_options[:host] = "https://gentle-inlet-80064.herokuapp.com"
 end
