f2b4a272243359178680983724b148684bf6d05e
Anbazhagan <sasikumar8994@gmail.com>
2017-06-03 08:22:37 +0530

    Product menu relationship added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   paginates_per 10
   has_many :pictures
+  belongs_to :menu
   accepts_nested_attributes_for :pictures, reject_if: :all_blank, allow_destroy: true
 end
