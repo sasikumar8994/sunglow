38ba7151382429fcb085d406f13bdd12b9b2497a
Anbazhagan <sasikumar8994@gmail.com>
2017-06-04 16:36:52 +0530

    Froala editor enabled and deploy file changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :pty, true
 
 # set :linked_files, %w{config/database.yml, config/secrets.yml}
 set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :use_sudo, true
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
  set :keep_releases, 5
