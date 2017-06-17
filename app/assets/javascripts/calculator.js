9f6460c1f4a045808ab7a8376d0a930f35d3132f
Anbazhagan <sasikumar8994@gmail.com>
2017-06-17 22:45:09 +0530

    Calculator adding the currency

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -6,11 +6,12 @@ function showMoney(){
     var h = Number(document.getElementById("hourrange").value);
     var d = Number(document.getElementById("dayrange").value);
     var c = Number(document.getElementById("costPerKwh").value);
+    var cu = document.getElementById("currency").value;
     var res = h*d*n*(ow-rw)*c/1000;
     if(isNaN(res))
         document.getElementById("savedmoney").innerHTML = "<br />Sorry! We can't do the calculation! Please check if there is anything wrong with your input!<br />";
     else
-        document.getElementById("savedmoney").innerHTML = "<br />Congrats! You will save $" + res + " on power supply every year with this purchase!<br />";
+        document.getElementById("savedmoney").innerHTML = "<br />Congrats! You will save " + cu + ' '+res + " on power supply every year with this purchase!<br />";
 }
 function showValuehour(newValue){
     document.getElementById("rangehour").innerHTML=newValue;
